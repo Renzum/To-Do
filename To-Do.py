e5671dbdc4d3519af1e32999dc6a1b79580c664a
Karen Arzumanyan <garen.arzumanyan@gmail.com>
2022-03-29 03:55:12 +0200

    Finalized the foundation of the application

--- To-Do.py
+++ To-Do.py
@@ -2,18 +2,60 @@
 
 from sys import argv
 import getopt
+from os import environ
 
 to_do_list = []
 
-def print_todo_list():
-    for i in range(len(to_do_list)):
-        print("TODO: [" + str(i) + "] " + to_do_list[i])
+#Read the todo list from the user data file
+def read_to_do_file(path):
+    #Read the file at the given path
+    #Add each line to the todo list
+    try:
+        file = open(path, 'r')
+        for line in file:
+            to_do_list.append(line.strip())
+        file.close()
+    except FileNotFoundError:
+        try:
+            file = open(path, 'w')
+            file.close()
+        except:
+            print("Error creating file")
+            exit(1)
+    pass
 
+#Write the todo list to the user data file
+def write_to_do_file(path):
+    try:
+        file = open(path, 'w')
+        for line in to_do_list:
+            file.write(line + '\n')
+        file.close()
+    except FileNotFoundError:
+        print("File not found")
+        exit(1)
+    pass
+
+#Print the todo list in an [index] [job] format
+def print_todo_list():
+    if len(to_do_list) == 0:
+        print("No To-Do's! Job well done!")
+    else:
+        try:
+            for index, job in enumerate(to_do_list):
+                print("[{}] {}".format(index, job))
+        except IndexError:
+            print("Index out of range")
+            exit(1)
+
+#Add a list of words to the todo list as a single item
 def add_todo(todo):
+    #Concatenate all the words into a single string
     job = ''
     for word in todo:
         job += word + ' '
 
+    #Add the job to the todo list
     try:
         to_do_list.append(job)
     except :
@@ -22,6 +64,7 @@ def add_todo(todo):
 
     print("TODO added: " + job)
 
+#Remove a job from the todo list at a given index
 def remove_todo(index):
     #Check if index is an integer
     try:
@@ -30,12 +73,17 @@ def remove_todo(index):
         print("Index must be an integer")
         exit(1)
 
+    to_do_list.pop(index)
+
     print("TODO: remove todo number" . index)
 
 def print_help():
     pass
 
 def main(arguments):
+    data_file = environ['HOME'] + '/todo.txt'
+    #Read the todo list from the user data file
+    read_to_do_file(data_file)
     if len(arguments) == 0:
         print_todo_list()
         exit(0)
@@ -50,7 +98,8 @@ def main(arguments):
             print("Unknown command")
             exit(1)
 
+    #Write the todo list to the user data file
+    write_to_do_file(data_file)
+
 if __name__ == "__main__":
-    read_to_do_file()
     main(argv[1:])
-    write_to_do_file()
